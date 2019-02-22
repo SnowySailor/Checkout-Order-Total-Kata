61d7749db89f7fb54d570a940ae1d43441e93b4a
Nick Kanel <kanel.nick@gmail.com>
2019-02-22 15:12:22 -0500

    Fix ClientTests test functions to post data

--- tests/clienttests.py
+++ tests/clienttests.py
@@ -9,10 +9,10 @@ class ClientTests(unittest.TestCase):
 
     def test_data_store_when_given_valid_data_returns_200(self):
         post_data = {'key': 'key123', 'value': 'value456'}
-        r = requests.post('http://localhost:19546/datastore')
+        r = requests.post('http://localhost:19546/datastore', data=post_data)
         self.assertEqual(r.status_code, 200)
 
     def test_data_store_when_not_given_key_returns_400(self):
         post_data = {'value': 'value456'}
-        r = requests.post('http://localhost:19546/datastore')
+        r = requests.post('http://localhost:19546/datastore', data=post_data)
         self.assertEqual(r.status_code, 400)
