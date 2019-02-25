44a6ffab2bd4eec86a01abaa04fbaa42b98244bb
Nick Kanel <kanel.nick@gmail.com>
2019-02-24 23:54:09 -0500

    Add ServerTests calculate_best_savings test for BuyAgetBforC

--- src/models/specials/BuyAgetBforCoff.py
+++ src/models/specials/BuyAgetBforCoff.py
@@ -1,4 +1,5 @@
 from src.helpers import get_value
+import math
 
 # Buy 1, get 1 for 50% off (limit 4)
 class BuyAgetBforCoff:
@@ -19,6 +20,8 @@ class BuyAgetBforCoff:
         return d
 
     def calculate_best_savings(self, applied_to_item, items, datastore):
+        item = datastore.get('itemdetails:' + get_value(applied_to_item, 'name'))
+
         # If the number of items you have to buy is greater than the limit,
         # there is no way this special can apply to anything
         if self.limit is not None and self.buy + self.get > self.limit:
@@ -27,7 +30,15 @@ class BuyAgetBforCoff:
         amount = get_value(applied_to_item, 'amount', 0)
         # If the customer hasn't bought the minimum amount, there is no
         # way there can be any savings
-        if amount < self.buy:
+        if amount < self.buy + self.get:
             return 0
 
-        return 0
+        # How many items are involved in each application of this special
+        chunk_size = self.buy + self.get
+        # How many applications of this special can there be?
+        chunks = math.floor(amount / chunk_size)
+
+        # Return the number of applications times the total percentage
+        # discount off a single item times the price of a single item
+        savings = chunks * self.off * self.get * item.price
+        return savings
--- tests/servertests.py
+++ tests/servertests.py
@@ -1,5 +1,6 @@
 import unittest
 from src.models.item import Item
+from src.database import DataStore
 
 def MakeServerTests(baseurl):
     class ServerTests(unittest.TestCase):
@@ -11,8 +12,24 @@ def MakeServerTests(baseurl):
                 'off': 0.5
             }
             item = Item('test', 5.00, 'unit', special)
+            datastore = DataStore()
+            datastore.set('itemdetails:' + item.name, item)
 
-            savings = item.special.calculate_best_savings({'name': 'test', 'amount': 1}, [], None)
+            savings = item.special.calculate_best_savings({'name': 'test', 'amount': 1}, [], datastore)
             self.assertEqual(savings, 0)
+
+        def test_calculate_best_savings_for_BuyAgetBforCoff_for_item_with_5_amount_returns_correct_savings(self):
+            special = {
+                'type': 'buyAgetBforCoff',
+                'buy': 1,
+                'get': 2,
+                'off': 0.5
+            }
+            item = Item('test', 5.00, 'unit', special)
+            datastore = DataStore()
+            datastore.set('itemdetails:' + item.name, item)
+
+            savings = item.special.calculate_best_savings({'name': 'test', 'amount': 5}, [], datastore)
+            self.assertEqual(savings, 5.0)
 
     return ServerTests
