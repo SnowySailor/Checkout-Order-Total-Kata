3761e34ff9c237237a5fe95e1c0564fb466e4887
Nick Kanel <kanel.nick@gmail.com>
2019-03-02 23:39:42 -0500

    Add ServerTests for calculate_best_savings for AforB

--- tests/servertests.py
+++ tests/servertests.py
@@ -227,6 +227,19 @@ def MakeServerTests(baseurl):
             savings = item.special.calculate_best_savings({'name': 'soup', 'amount': 8}, [], datastore)
             self.assertEqual(savings, 5.00)
 
+        def test_calculate_best_savings_for_AforB_with_10_for_5_with_9_items_returns_0(self):
+            special = {
+                'type': 'AforB',
+                'buy': 10,
+                'for': 5.00,
+            }
+
+            datastore = DataStore()
+            item  = self.create_item('soup', 2.00, 'unit', special, datastore)
+
+            savings = item.special.calculate_best_savings({'name': 'soup', 'amount': 9}, [], datastore)
+            self.assertEqual(savings, 0.00)
+
         def create_item(self, name, price, billing_method, special, datastore):
             item = Item(name, price, billing_method, special)
             datastore.set('itemdetails:' + name, item)
