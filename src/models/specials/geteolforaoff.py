cb65cc8986e75c5c8374eeb78c51f6a8dab67782
Nick Kanel <kanel.nick@gmail.com>
2019-03-02 22:48:03 -0500

    Add ServerTests for calculate_best_savings for GetEOLforAoff

--- src/models/specials/geteolforaoff.py
+++ src/models/specials/geteolforaoff.py
@@ -20,10 +20,24 @@ class GetEOLforAoff:
         # Find the highest cost item that is less than the max_cost
         highest_cost = 0
         for item in items:
+            # Get the definition for the item we're currently looking at
             current_item_def = datastore.get('itemdetails:' + get_value(item, 'name'))
-            current_item_price = get_value(item, 'amount') * current_item_def.price
-            if current_item_price > highest_cost and current_item_price <= max_cost:
-                highest_cost = current_item_price
+            current_item_amount = get_value(item, 'amount')
+
+            # Figure out how many of the current items we can fit in while still being
+            # below the max_cost
+            max_current_item_count = 0
+            for i in range(1, current_item_amount + 1):
+                if i * current_item_def.price <= max_cost:
+                    max_current_item_count = i
+
+            if max_current_item_count == 0:
+                continue
+
+            max_current_item_price = max_current_item_count * current_item_def.price
+            # Determine if it's better than any previous item we've seen
+            if max_current_item_price > highest_cost:
+                highest_cost = max_current_item_price
 
         savings = self.off/100 * highest_cost
         return round(savings, 2)
