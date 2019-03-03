cf36f790e8313868253c2623b279f93cc50a0579
SnowySailor <kanel.nick@gmail.com>
2019-03-03 12:51:28 -0500

    Add ServerTests for calculate_total

--- tests/servertests.py
+++ tests/servertests.py
@@ -366,7 +366,7 @@ def MakeServerTests(baseurl):
             total = order.calculate_total()
             self.assertEqual(total, 0.00)
 
-        def test_calculate_total_for_order_with_one_special_and_4_items(self):
+        def test_calculate_total_for_order_with_one_markdown_special_and_4_items(self):
             datastore = DataStore()
             special = {
                 'type': 'markdown',
@@ -387,7 +387,7 @@ def MakeServerTests(baseurl):
             total = order.calculate_total()
             self.assertEqual(total, 109.83)
 
-        def test_calculate_total_for_order_with_two_specials_and_4_items(self):
+        def test_calculate_total_for_order_with_two_markdown_specials_and_4_items(self):
             datastore = DataStore()
             special1 = {'type': 'markdown','percentage': 25,'limit': 6}
             special2 = {'type': 'markdown','percentage': 50}
@@ -404,6 +404,23 @@ def MakeServerTests(baseurl):
 
             total = order.calculate_total()
             self.assertEqual(total, 108.94)
+
+        def test_calculate_total_for_order_with_one_AforB_special_and_4_items(self):
+            datastore = DataStore()
+            special1 = {'type': 'AforB', 'buy': 2, 'for': 2.00}
+            item  = self.create_item('soup', 2.00, 'unit', special1, datastore)
+            item2 = self.create_item('peas', 1.78, 'unit', None, datastore)
+            item3 = self.create_item('chicken', 2.37, 'weight', None, datastore)
+            item4 = self.create_item('beef', 9.99, 'weight', None, datastore)
+            order = self.create_order(datastore)
+
+            order.add_item(item, 4)
+            order.add_item(item2, 1)
+            order.add_item(item3, 4.32)
+            order.add_item(item4, 9.34)
+
+            total = order.calculate_total()
+            self.assertEqual(total, 109.33)
 
         def create_item(self, name, price, billing_method, special, datastore):
             item = Item(name, price, billing_method, special)
